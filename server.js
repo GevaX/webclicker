36496cf690b6ef447dffc4f1049ff3c8796e6a6b
GevaX <gevax1010@gmail.com>
2024-03-23 21:20:12 +0200

    23/3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname + "/public/")));
 const Pusher = require('pusher');
 require('dotenv').config();
 
